d3fb34a944ba857cce8495b4e5283f35d1f2e58f
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-08-11 23:30:29 +0200

    Fade in loading indicator

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Spinner } from "react-bootstrap";
+import { Spinner, Fade } from "react-bootstrap";
 
-import './Loading.css';
+import "./Loading.css";
 
 function Loading() {
   return (
-    <div className="Loading">
-      <Spinner animation="grow" role="status">
-        <span className="sr-only">Loading...</span>
-      </Spinner>
-      <div>Laster...</div>
-    </div>
+    <Fade appear in>
+      <div className="Loading">
+        <Spinner animation="grow" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+        <div>Laster...</div>
+      </div>
+    </Fade>
   );
 }
 
