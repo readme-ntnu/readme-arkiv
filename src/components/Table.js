5ab981b0d3c5a7bff6a37b644d2f544e92134f48
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-08-11 23:35:16 +0200

    Added fade to table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,34 +1,36 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Table, Fade } from "react-bootstrap";
 
 function AppTable(props) {
   return (
-    <Table striped bordered hover className="search-table">
-      <thead>
-        <tr>
-          <th>Utgave</th>
-          <th>Tittel</th>
-          <th>Forfatter</th>
-          <th>Layout</th>
-          <th>Spalte</th>
-          <th>Stikkord</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.articles.map(article => (
-          <tr key={article._id}>
-            <td>
-              <a href={article.url}>{article.edition}</a>
-            </td>
-            <td>{article.title}</td>
-            <td>{article.author}</td>
-            <td>{article.layout}</td>
-            <td>{article.type}</td>
-            <td>{article.tags}</td>
+    <Fade appear in>
+      <Table striped bordered hover className="search-table">
+        <thead>
+          <tr>
+            <th>Utgave</th>
+            <th>Tittel</th>
+            <th>Forfatter</th>
+            <th>Layout</th>
+            <th>Spalte</th>
+            <th>Stikkord</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {props.articles.map(article => (
+            <tr key={article._id}>
+              <td>
+                <a href={article.url}>{article.edition}</a>
+              </td>
+              <td>{article.title}</td>
+              <td>{article.author}</td>
+              <td>{article.layout}</td>
+              <td>{article.type}</td>
+              <td>{article.tags}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Fade>
   );
 }
 
