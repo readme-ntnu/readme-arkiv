cbaf384404f6b820bc3cb1bdd34d7e44133d1ad8
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-09-16 10:40:19 +0200

    Added PDFs to images, styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,55 +10,74 @@ import * as firebase from "firebase";
 function Home() {
   const [images, setImages] = useState([]);
 
-  const [loading, setLoading] = useState(false);
+  const [downloading, setDownloading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
+      setDownloading(true);
       var loadImg = [];
       let storage = firebase.storage();
       let storageRef = storage.ref("images");
       let list = await storageRef.listAll();
       let arrayOfPromises = [];
-      let items = list.prefixes;
+      let items = list.prefixes.reverse();
       for (let i = 0; i < items.length; i++) {
-        arrayOfPromises.push(fetchImagesForYear(items[i]));
+        arrayOfPromises.push(fetchDataForYear(items[i]));
       }
       let responses = await Promise.all(arrayOfPromises);
       for (var yearObj of responses) {
         loadImg.push(yearObj);
       }
       setImages(loadImg);
+      setDownloading(false);
     }
-    setLoading(true);
     fetchData();
-    setLoading(false);
   }, []);
 
   let imgRows = images.map(URLsrow => {
     return <ImageRow info={URLsrow} key={URLsrow.year} />;
   });
 
-  imgRows.reverse();
-
-  return loading ? (
-    <Spinner animation="grow" />
-  ) : (
-    <div className="row-container">{imgRows}</div>
-  );
+  if (downloading) {
+    return <Spinner animation="grow" />;
+  } else {
+    return <div className="row-container">{imgRows}</div>;
+  }
+}
+async function fetchDataForYear(yearPrefix) {
+  let object = {};
+  let response = await Promise.all([
+    fetchImagesForAYear(yearPrefix),
+    fetchPDFsForAYear(yearPrefix)
+  ]);
+  object["year"] = yearPrefix.name;
+  object["urls"] = response[0];
+  object["pdfs"] = response[1];
+  return object;
 }
 
-async function fetchImagesForYear(yearPrefix) {
+async function fetchImagesForAYear(yearPrefix) {
   let imgRefs = await yearPrefix.list();
-  let imgRefsItems = imgRefs.items;
-  let object = {};
+  let imgRefsItems = imgRefs.items.reverse();
   let urls = [];
   for (let j = 0; j < imgRefsItems.length; j++) {
     let res = await imgRefsItems[j].getDownloadURL();
     urls.push(res);
   }
-  object["year"] = yearPrefix.name;
-  object["urls"] = urls;
-  return object;
+  return urls;
+}
+
+async function fetchPDFsForAYear(yearPrefix) {
+  let storage = firebase.storage();
+  let year = yearPrefix.name;
+  let PDFrefs = await storage.ref("pdf/" + year).list();
+  let PDFrefsItems = PDFrefs.items.reverse();
+  let pdfUrls = [];
+  for (let i = 0; i < PDFrefsItems.length; i++) {
+    let url = await PDFrefsItems[i].getDownloadURL();
+    pdfUrls.push(url);
+  }
+  return pdfUrls;
 }
 
 export default Home;
--- src/components/ImageRow.js
+++ src/components/ImageRow.js
@@ -1,23 +1,32 @@
 import React from "react";
 
-import { Image } from "react-bootstrap";
+import { Image, Fade } from "react-bootstrap";
+import "firebase/storage";
 
 import "./ImageRow.css";
 
 function ImageRow(props) {
-  let images = props.info.urls.map(url => {
-    return <Image className="RowImage" src={url} key={url} fluid />;
+  let images = props.info.urls.map((url, index) => {
+    return (
+      <div className="RowImage" key={url}>
+        <a href={props.info.pdfs[index]}>
+          <Image src={url} fluid />
+        </a>
+      </div>
+    );
   });
 
   let imagesLen = images.length;
   let images1 = images.slice(0, imagesLen - 3);
   let images2 = images.slice(imagesLen - 3, imagesLen);
   return (
-    <div className="ImageRow">
-      <h2>{props.info.year}</h2>
-      <div className="row">{images1}</div>
-      <div className="row">{images2}</div>
-    </div>
+    <Fade appear in>
+      <div className="ImageRow">
+        <h2 className="year">{props.info.year}</h2>
+        <div className="row">{images1}</div>
+        <div className="row">{images2}</div>
+      </div>
+    </Fade>
   );
 }
 
