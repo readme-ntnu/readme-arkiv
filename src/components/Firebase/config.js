cd0cac7e56a7a893430dc9909f0cff598b084a93
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-09-16 13:35:46 +0200

    Hide yo keys!

--- src/components/Firebase/config.js
+++ src/components/Firebase/config.js
@@ -1,11 +1,11 @@
 const config = {
-  apiKey: "AIzaSyCLhIKGOYZilQuXirB_W-1UmKNfQygETqw",
-  authDomain: "readme-arkiv.firebaseapp.com",
-  databaseURL: "https://readme-arkiv.firebaseio.com",
-  projectId: "readme-arkiv",
-  storageBucket: "readme-arkiv.appspot.com",
-  messagingSenderId: "884912593534",
-  appId: "1:884912593534:web:994587a01eb1bd1d85d62f"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export default config;
