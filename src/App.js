2df3e45286aff0fcee9a5a851f08d2c331a4cdf0
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-09-22 16:47:22 +0200

    Added 404-page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Image } from "react-bootstrap";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import AppNavBar from "./components/Navbar";
 import Home from "./components/Home.js";
 import Search from "./components/Search.js";
+import NoMatch from "./components/NoMatch.js";
 
 function App() {
   return (
@@ -15,8 +16,11 @@ function App() {
         <header className="App-header">
           <Image id="readmelogo" src="readme.png" alt="Logo" fluid />
         </header>
-        <Route exact path="/" component={Home} />
-        <Route path="/search" component={Search} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/search" component={Search} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
     </Router>
   );
