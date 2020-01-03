577b3063c6ffabddc2619d83609d6054f97550fd
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-01-03 14:59:01 +0100

    Refactored firebase service to eliminate drilling

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -39,180 +39,181 @@ class Firebase {
   articles = () => this.db.collection("articles");
 
   addArticle = (article, callback = undefined) =>
-    addArticleToDB(article, callback, this.db, this.storage);
+    this.addArticleToDB(article, callback);
 
   // *** Editions API ***
-  editions = year => fetchEditionDataForYear(year, this.storage);
+  editions = year => this.fetchEditionDataForYear(year);
 
-  editionYearPrefixes = () => fetchYearPrefixes(this.storage);
+  editionYearPrefixes = () => this.fetchYearPrefixes();
 
   uploadEdition = (editionFile, listinglop, callback = undefined) =>
-    doEditionUpload(editionFile, listinglop, callback, this.storage, this.db);
+    this.doEditionUpload(editionFile, listinglop, callback);
 
   // *** Settings API ***
-  getSettings = () => fetchSettings(this.db);
+  getSettings = () => this.fetchSettings();
 
-  setShowListing = value => setShowListingSetting(value, this.db);
-}
+  setShowListing = value => this.setShowListingSetting(value);
 
-async function fetchYearPrefixes(storage) {
-  const list = await storage.ref("images").listAll();
-  return list.prefixes.reverse();
-}
+  // *** Helper functions ***
+  fetchYearPrefixes = async () => {
+    const list = await this.storage.ref("images").listAll();
+    return list.prefixes.reverse();
+  };
 
-async function fetchEditionDataForYear(yearPrefix, storage) {
-  let object = {};
-  let response = await Promise.all([
-    fetchImagesForAYear(yearPrefix),
-    fetchPDFsForAYear(yearPrefix, storage)
-  ]);
-  object.year = yearPrefix.name;
-  object.urls = response[0];
-  object.pdfs = response[1];
-  return object;
-}
+  fetchEditionDataForYear = async yearPrefix => {
+    let object = {};
+    let response = await Promise.all([
+      this.fetchImagesForAYear(yearPrefix),
+      this.fetchPDFsForAYear(yearPrefix)
+    ]);
+    object.year = yearPrefix.name;
+    object.urls = response[0];
+    object.pdfs = response[1];
+    return object;
+  };
 
-async function fetchImagesForAYear(yearPrefix) {
-  let imgRefs = await yearPrefix.list();
-  let imgRefsItems = imgRefs.items.reverse();
-  let urls = await Promise.all(imgRefsItems.map(ref => ref.getDownloadURL()));
-  return urls;
-}
+  fetchImagesForAYear = async yearPrefix => {
+    let imgRefs = await yearPrefix.list();
+    let imgRefsItems = imgRefs.items.reverse();
+    let urls = await Promise.all(imgRefsItems.map(ref => ref.getDownloadURL()));
+    return urls;
+  };
 
-async function fetchPDFsForAYear(yearPrefix, storage) {
-  let year = yearPrefix.name;
-  let PDFRefs = await storage.ref("pdf/" + year).list();
-  let PDFRefsItems = PDFRefs.items.reverse();
-  const pdfUrls = Promise.all(
-    PDFRefsItems.map(async ref => {
-      const dataFromServer = await ref
-        .getMetadata()
-        .then(data => data.customMetadata.listinglop)
-        .catch(error => {
-          console.warn("Fond no metadata with error: ", error);
-          return false;
+  fetchPDFsForAYear = async yearPrefix => {
+    let year = yearPrefix.name;
+    let PDFRefs = await this.storage.ref("pdf/" + year).list();
+    let PDFRefsItems = PDFRefs.items.reverse();
+    const pdfUrls = Promise.all(
+      PDFRefsItems.map(async ref => {
+        const dataFromServer = await ref
+          .getMetadata()
+          .then(data => data.customMetadata.listinglop)
+          .catch(error => {
+            console.warn("Fond no metadata with error: ", error);
+            return false;
+          });
+        return {
+          listinglop: dataFromServer === "true",
+          url: await ref.getDownloadURL()
+        };
+      })
+    );
+    return pdfUrls;
+  };
+
+  doEditionUpload = async (editionFile, listinglop, callback) => {
+    const year = editionFile.name.split("-")[0];
+    const path = `pdf/${year}/${editionFile.name}`;
+    const metadata = {
+      contentType: "application/pdf",
+      customMetadata: {
+        listinglop: String(listinglop)
+      }
+    };
+    const editionPDFRef = this.storage.ref(path);
+    const uploadTask = editionPDFRef.put(editionFile, metadata);
+    uploadTask.on(
+      app.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+      function(snapshot) {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+      },
+      function(error) {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/unauthorized":
+            throw Error("Error, not authorized for file upload");
+
+          case "storage/unknown":
+            throw Error("Unkown error, file upload failed.");
+          default:
+        }
+      },
+      function() {
+        // Upload completed successfully, now we can get the download URL
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+          console.log("File available at", downloadURL);
+          const editionName = editionFile.name.replace(".pdf", "");
+          this.updateArticlePDFURL(editionName, downloadURL);
         });
-      return {
-        listinglop: dataFromServer === "true",
-        url: await ref.getDownloadURL()
-      };
-    })
-  );
-  return pdfUrls;
-}
+        if (callback && typeof callback === "function") {
+          callback();
+        }
+      }
+    );
+  };
 
-async function doEditionUpload(editionFile, listinglop, callback, storage, db) {
-  const year = editionFile.name.split("-")[0];
-  const path = `pdf/${year}/${editionFile.name}`;
-  const metadata = {
-    contentType: "application/pdf",
-    customMetadata: {
-      listinglop: String(listinglop)
+  updateArticlePDFURL = async (editionName, newURL) => {
+    const articles = await this.db
+      .collection("articles")
+      .where("edition", "==", editionName)
+      .get();
+    if (articles && !articles.empty) {
+      articles.forEach(async docSnap => {
+        await docSnap.ref.update({
+          url: `${newURL}#page=${this.getPageNumber(docSnap.data())}`
+        });
+      });
     }
   };
-  const editionPDFRef = storage.ref(path);
-  const uploadTask = editionPDFRef.put(editionFile, metadata);
-  uploadTask.on(
-    app.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-    function(snapshot) {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log("Upload is " + progress + "% done");
-    },
-    function(error) {
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case "storage/unauthorized":
-          throw Error("Error, not authorized for file upload");
 
-        case "storage/unknown":
-          throw Error("Unkown error, file upload failed.");
-        default:
-      }
-    },
-    function() {
-      // Upload completed successfully, now we can get the download URL
-      uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-        console.log("File available at", downloadURL);
-        const editionName = editionFile.name.replace(".pdf", "");
-        updateArticlePDFURL(editionName, downloadURL, db);
-      });
+  addArticleToDB = async (article, callback) => {
+    try {
+      const url = await this.getArticlePDFURL(article);
+      article.url = url;
+      await this.db.collection("articles").add(article);
+      console.log("Article added to DB");
       if (callback && typeof callback === "function") {
         callback();
       }
+    } catch (error) {
+      console.error(
+        "Something when wrong during edition upload, failed with error: ",
+        error
+      );
     }
-  );
-}
+  };
 
-async function updateArticlePDFURL(editionName, newURL, db) {
-  const articles = await db
-    .collection("articles")
-    .where("edition", "==", editionName)
-    .get();
-  if (articles && !articles.empty) {
-    articles.forEach(async docSnap => {
-      await docSnap.ref.update({
-        url: `${newURL}#page=${getPageNumber(docSnap.data())}`
-      });
+  getArticlePDFURL = async article => {
+    const year = article.edition.split("-")[0];
+    const fileName = `${article.edition}.pdf`;
+    const path = `pdf/${year}/${fileName}`;
+    try {
+      let pdfURL = await this.storage.ref(path).getDownloadURL();
+      if (article.pages) {
+        pdfURL = `${pdfURL}#page=${this.getPageNumber(article)}`;
+      }
+      return pdfURL;
+    } catch (error) {
+      throw Error("Failed to get article PDF URL, with error: ", error);
+    }
+  };
+
+  getPageNumber = article => {
+    const [editionYear, editionNumber] = article.edition.split("-");
+    if (
+      editionYear > 2013 ||
+      (parseInt(editionYear) === 2013 && parseInt(editionNumber) === 6)
+    ) {
+      return Math.floor(article.pages[0] / 2) + 1;
+    } else {
+      return article.pages[0];
+    }
+  };
+
+  fetchSettings = async () => {
+    const settings = await this.db.collection("settings").get();
+    return settings.docs[0].data();
+  };
+
+  setShowListingSetting = async value => {
+    const settings = await this.db.collection("settings").get();
+    await settings.docs[0].ref.update({
+      showListing: value
     });
-  }
-}
-
-async function addArticleToDB(article, callback, db, storage) {
-  try {
-    const url = await getArticlePDFURL(article, storage);
-    article.url = url;
-    await db.collection("articles").add(article);
-    console.log("Article added to DB");
-    if (callback && typeof callback === "function") {
-      callback();
-    }
-  } catch (error) {
-    console.error(
-      "Something when wrong during edition upload, failed with error: ",
-      error
-    );
-  }
-}
-
-async function getArticlePDFURL(article, storage) {
-  const year = article.edition.split("-")[0];
-  const fileName = `${article.edition}.pdf`;
-  const path = `pdf/${year}/${fileName}`;
-  try {
-    let pdfURL = await storage.ref(path).getDownloadURL();
-    if (article.pages) {
-      pdfURL = `${pdfURL}#page=${getPageNumber(article)}`;
-    }
-    return pdfURL;
-  } catch (error) {
-    throw Error("Failed to get article PDF URL, with error: ", error);
-  }
-}
-
-function getPageNumber(article) {
-  const [editionYear, editionNumber] = article.edition.split("-");
-  if (
-    editionYear > 2013 ||
-    (parseInt(editionYear) === 2013 && parseInt(editionNumber) === 6)
-  ) {
-    return Math.floor(article.pages[0] / 2) + 1;
-  } else {
-    return article.pages[0];
-  }
-}
-
-async function fetchSettings(db) {
-  const settings = await db.collection("settings").get();
-  return settings.docs[0].data();
-}
-
-async function setShowListingSetting(value, db) {
-  const settings = await db.collection("settings").get();
-  await settings.docs[0].ref.update({
-    showListing: value
-  });
+  };
 }
 
 export default Firebase;
