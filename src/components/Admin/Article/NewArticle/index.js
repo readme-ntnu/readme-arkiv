83de6c6d587890dfe4ccd1cb4057da2fe5659b69
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-02-07 13:30:27 +0100

    Fixed a minor edition upload bug

--- src/components/Admin/Article/NewArticle/index.js
+++ src/components/Admin/Article/NewArticle/index.js
@@ -5,7 +5,9 @@ import { withAuthorization } from "../../../Session";
 import ArticleForm from "../ArticleForm";
 
 function NewArticlePage({ firebase }) {
-  function doHandleSubmit(values, { setSubmitting, setStatus }) {
+  function doHandleSubmit(valuesToSubmit, { setSubmitting, setStatus }) {
+    // Making a true copy to avoid pass-by-reference issues
+    const values = JSON.parse(JSON.stringify(valuesToSubmit));
     values.pages = values.pages.split(",").map(v => parseInt(v));
     values.tags = values.tags.split(",").map(v => v.trim());
     values.edition = `${values.editionYear}-0${values.editionNumber}`;
