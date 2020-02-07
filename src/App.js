c7413294cc581e4a344474ca556ccbe67f8e0081
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-02-07 16:35:13 +0100

    added a light switch for dark mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Image } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
+
+import { app, readmelogo, lightSwitch, header } from "./App.module.css";
 
 import * as ROUTES from "./constants/routes";
 
@@ -16,6 +17,7 @@ import EditionList from "./components/Admin/Edition/EditionList";
 import NewArticlePage from "./components/Admin/Article/NewArticle";
 import EditArticle from "./components/Admin/Article/EditArticle";
 import ArticleList from "./components/Admin/Article/ArticleList";
+import LightSwitch from "./components/LightSwitch";
 
 import NoMatch from "./components/NoMatch";
 import { withAuthentication } from "./components/Session";
@@ -23,15 +25,18 @@ import { withAuthentication } from "./components/Session";
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className={app}>
         <AppNavBar />
-        <header className="App-header">
+        <header className={header}>
           <Image
-            id="readmelogo"
+            className={readmelogo}
             src={`${process.env.PUBLIC_URL}/readme.png`}
             alt="Logo"
             fluid
           />
+          <div className={lightSwitch}>
+            <LightSwitch />
+          </div>
         </header>
         <Switch>
           <Route exact path={ROUTES.HOME} component={Home} />
