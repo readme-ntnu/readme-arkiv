ed921db616acbb74c7561fcaf4cac483dbdd0275
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-03-08 14:54:03 +0100

    Added a proper listing toggle, replaced the old button

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -5,13 +5,13 @@ import { Fade, Spinner } from "react-bootstrap";
 
 import "./Loading.css";
 
-function Loading({ styles }) {
+function Loading({ styles, msg = "Laster..." }) {
   const darkmode = useDarkMode();
   return (
     <Fade appear in>
       <div className="Loading" style={styles || {}}>
         <Spinner animation="grow" variant={darkmode.value ? "light" : "dark"} />
-        <div>Laster...</div>
+        <div>{msg}</div>
       </div>
     </Fade>
   );
