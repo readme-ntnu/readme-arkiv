64f1ab6e9f45d45afa996816aa1df36f87f4d3fd
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-09-10 19:38:15 +0200

    Added a footer with tiny readme logo

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import useDarkMode from "use-dark-mode";
 import CrossfadeImage from "react-crossfade-image";
 
-import { app, readmelogo, header } from "./App.module.css";
+import { app, readmelogo, header, content } from "./App.module.css";
 
 import * as ROUTES from "./constants/routes";
 
 import AppNavBar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Search from "./components/Search";
 import SignInPage from "./components/SignIn";
@@ -37,19 +38,25 @@ function App() {
             <CrossfadeImage src={logoSrc} alt="Logo" />
           </div>
         </header>
-        <Switch>
-          <Route exact path={ROUTES.HOME} component={Home} />
-          <Route path={ROUTES.SEARCH} component={Search} />
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-          <Route path={ROUTES.ADMIN} component={AdminPage} />
-          <Route path={ROUTES.NEW_EDITION} component={NewEditionPage} />
-          <Route path={ROUTES.EDITION_LIST} component={EditionList} />
-          <Route path={ROUTES.NEW_ARTICLE} component={NewArticlePage} />
-          <Route path={ROUTES.EDIT_ARTICLE} component={EditArticle} />
-          <Route path={ROUTES.ARTICLE_LIST} component={ArticleList} />
-          <Route component={NoMatch} />
-        </Switch>
+        <div className={content}>
+          <Switch>
+            <Route exact path={ROUTES.HOME} component={Home} />
+            <Route path={ROUTES.SEARCH} component={Search} />
+            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+            <Route
+              path={ROUTES.PASSWORD_FORGET}
+              component={PasswordForgetPage}
+            />
+            <Route path={ROUTES.ADMIN} component={AdminPage} />
+            <Route path={ROUTES.NEW_EDITION} component={NewEditionPage} />
+            <Route path={ROUTES.EDITION_LIST} component={EditionList} />
+            <Route path={ROUTES.NEW_ARTICLE} component={NewArticlePage} />
+            <Route path={ROUTES.EDIT_ARTICLE} component={EditArticle} />
+            <Route path={ROUTES.ARTICLE_LIST} component={ArticleList} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
